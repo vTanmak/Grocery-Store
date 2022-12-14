d59fdfa32b6fdce563c893a5aa77d0dce4b555b5
vTanmak <107548734+vTanmak@users.noreply.github.com>
2022-12-14 23:34:31 +0530

    Updated admin.py
    Updated the search feature.

--- admin.py
+++ admin.py
@@ -7,7 +7,18 @@ cursor = connection.cursor()
 
 def Add_Product():
     sql = "INSERT INTO PRODUCTS VALUES(%s,%s,%s)"
+
     product_id = (int(input("Enter Product ID: ")))
+    cursor.execute("SELECT product_id from products")
+    data = cursor.fetchall()
+    exist = False
+    for i in data:
+        if i[0] == product_id:
+            exist = True
+    if exist:
+        print("A product already exist with that product ID")
+        print()
+        return
     product_name = (input("Enter name of the Product: "))
     product_price = (float(input("Enter price of the Product: ")))
     value = [product_id,product_name,product_price]
@@ -17,7 +28,15 @@ def Add_Product():
     print()
 
 def Delete_Product():
-    search = (int(input("Enter the product id: ")))
+    L = [["Product ID","Product Name","Price Per Unit"]]
+    sql = "SELECT * from products"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for i in data:
+        temp_list = [i[0],i[1],i[2]]
+        L.append(temp_list)
+    print(tabulate((L),headers="firstrow",tablefmt="psql"))
+    search = (int(input("Enter the product id of the product to delete: ")))
     found = False
     cursor.execute("SELECT * FROM PRODUCTS")
     data = cursor.fetchall()
@@ -35,6 +54,14 @@ def Delete_Product():
     print()
     
 def Update_Product():
+    L = [["Product ID","Product Name","Price Per Unit"]]
+    sql = "SELECT * from products"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for i in data:
+        temp_list = [i[0],i[1],i[2]]
+        L.append(temp_list)
+    print(tabulate((L),headers="firstrow",tablefmt="psql"))
     search = (int(input("Enter the product id: ")))
     found = False
     cursor.execute("SELECT * FROM PRODUCTS;")
@@ -48,20 +75,20 @@ def Update_Product():
         sql = "UPDATE products SET price_per_unit= '{}' WHERE product_id = '{}'".format(new_price,search)
         cursor.execute(sql)
         connection.commit()
-        print("Product has been found and successfully update.")
+        print("Product has been found and successfully updated.")
     else:
         print("Product not found.")
     print()
 
-def Check_Last_Orders():
+def Check_Orders():
     print("Details of the order by the customer: ")
     print()
-    L = [["Order ID","Customer Name","Total Price","Date and Time"]]
+    L = [["Order ID","Customer ID","Customer Name","Total Price","Date and Time"]]
     sql = "SELECT * FROM orders"
     cursor.execute(sql)
     data = cursor.fetchall()
     for i in data:
-        temp_list = [i[0],i[1],i[2],i[3]]
+        temp_list = [i[0],i[1],i[2],i[3],i[4]]
         L.append(temp_list)
     print(tabulate((L),headers="firstrow",tablefmt="psql"))
     print()
@@ -71,7 +98,8 @@ def Search_Order_Details():
     print("I. Search by Order ID")
     print("II. Search by Price")
     print("III. Search by Name of the Customer")
-    print("IV. Search by Date.")
+    print("IV. Search by Date")
+    print("V. Search by Customer ID")
     ch = (input("Enter your choice: "))
     ch = ch.upper()
 
@@ -80,14 +108,15 @@ def Search_Order_Details():
         sql = "SELECT * FROM ORDERS WHERE ORDER_ID = '{}'".format(order_id,)
         cursor.execute(sql)
         data = cursor.fetchone()
-        print(data)
         if len(data) == 0:
             print("Product not found.")
         else:
-            name = data[1]
-            total = data[2]
-            datetime = data[3]
+            customer_id = data[1]
+            name = data[2]
+            total = data[3]
+            datetime = data[4]
             print()
+            print("Customer ID:",customer_id)
             print("Name: ",name)
             print("Total: ",total)
             print("Date and Time when purchased products: ",datetime)
@@ -108,9 +137,9 @@ def Search_Order_Details():
             if len(data) == 0:
                 print("No record found to display.")
             else:
-                L = [["Order ID","Customer Name","Price","Date Time"]]
+                L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
                 for i in data:
-                    temp_list = [i[0],i[1],i[2],i[3]]
+                    temp_list = [i[0],i[1],i[2],i[3],i[4]]
                     L.append(temp_list)
                 print(tabulate((L),headers="firstrow",tablefmt="psql"))
             print()
@@ -123,12 +152,13 @@ def Search_Order_Details():
             if len(data) == 0:
                 print("No record found to display.")
             else:
-                L = [["Order ID","Customer Name","Price","Date Time"]]
+                L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
                 for i in data:
-                    temp_list = [i[0],i[1],i[2],i[3]]
+                    temp_list = [i[0],i[1],i[2],i[3],i[4]]
                     L.append(temp_list)
                 print(tabulate((L),headers="firstrow",tablefmt="psql"))
         elif choi == "c":
+            print()
             upper_range = (int(input("Enter upper range of the price: ")))
             lower_range = (int(input("Enter lower range of the price: ")))
             sql = "SELECT * FROM ORDERS WHERE total BETWEEN {} AND {}".format(upper_range,lower_range)
@@ -137,9 +167,9 @@ def Search_Order_Details():
             if len(data) == 0:
                 print("No record found to display.")
             else:
-                L = [["Order ID","Customer Name","Price","Date Time"]]
+                L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
                 for i in data:
-                    temp_list = [i[0],i[1],i[2],i[3]]
+                    temp_list = [i[0],i[1],i[2],i[3],i[4]]
                     L.append(temp_list)
                 print(tabulate((L),headers="firstrow",tablefmt="psql"))
         else:
@@ -156,9 +186,9 @@ def Search_Order_Details():
             print("No order found.")
         else:
             print()
-            L = [["Order ID","Customer Name","Price","Date Time"]]
+            L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
             for i in data:
-                temp_list = [i[0],i[1],i[2],i[3]]
+                temp_list = [i[0],i[1],i[2],i[3],i[4]]
                 L.append(temp_list)
             print(tabulate((L),headers="firstrow",tablefmt="psql"))
 
@@ -166,7 +196,8 @@ def Search_Order_Details():
         print()
         print("a) Display all the orders which has been placed after the given date")
         print("b) Display all the orders which has been placed before the given date")
-        print("c) Search orders in range of date")
+        print("c) Display all the orders which has been placed on the given date")
+        print("d) Search orders in range of date")
         choi = (input("Enter your choice (a/b/c): "))
         choi = choi.lower()
         print()
@@ -178,9 +209,9 @@ def Search_Order_Details():
             if len(data) == 0:
                 print("No record found to display.")
             else:
-                L = [["Order ID","Customer Name","Price","Date Time"]]
+                L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
                 for i in data:  
-                    temp_list = [i[0],i[1],i[2],i[3]]
+                    temp_list = [i[0],i[1],i[2],i[3],i[4]]
                     L.append(temp_list)
                 print(tabulate((L),headers="firstrow",tablefmt="psql"))
         elif choi == "b":
@@ -191,12 +222,25 @@ def Search_Order_Details():
             if len(data) == 0:
                 print("No record found to display.")
             else:
-                L = [["Order ID","Customer Name","Price","Date Time"]]
+                L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
                 for i in data:
-                    temp_list = [i[0],i[1],i[2],i[3]]
+                    temp_list = [i[0],i[1],i[2],i[3],i[4]]
                     L.append(temp_list)
                 print(tabulate((L),headers="firstrow",tablefmt="psql"))
         elif choi == "c":
+            date_range = (input("Enter the date to display the orders placed on that date (Format should be: YYYY-MM-DD): "))
+            sql = "SELECT * FROM ORDERS WHERE DATE(datetime) = '{}'".format(date_range,)
+            cursor.execute(sql)
+            data = cursor.fetchall()
+            if len(data) == 0:
+                print("No record found to display.")
+            else:
+                L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
+                for i in data:
+                    temp_list = [i[0],i[1],i[2],i[3],i[4]]
+                    L.append(temp_list)
+                print(tabulate((L),headers="firstrow",tablefmt="psql"))
+        elif choi == "d":
             upper_range = (input("Enter the date to display the orders placed after that date (Format should be: YYYY-MM-DD): "))
             lower_range = (input("Enter the date to display the orders placed before that date (Format should be: YYYY-MM-DD): "))
             sql = "SELECT * FROM ORDERS WHERE datetime BETWEEN '{}' AND '{}'".format(upper_range,lower_range)
@@ -206,17 +250,32 @@ def Search_Order_Details():
             if len(data) == 0:
                 print("No record found to display.")
             else:
-                L = [["Order ID","Customer Name","Price","Date Time"]]
+                L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
                 for i in data:
-                    temp_list = [i[0],i[1],i[2],i[3]]
+                    temp_list = [i[0],i[1],i[2],i[3],i[4]]
                     L.append(temp_list)
                 print(tabulate((L),headers="firstrow",tablefmt="psql"))
         else:
             print("Invalid input.")
+    
+    elif ch == "V":
+        c_id = (int(input("Enter the cusomter id to search: ")))
+        sql = "SELECT * FROM ORDERS WHERE customer_id = {}".format(c_id)
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        if len(data) == 0:
+            print("No record found to display.")
+        else:
+            L = [["Order ID","Cusomter ID","Customer Name","Price","Date Time"]]
+            for i in data:  
+                temp_list = [i[0],i[1],i[2],i[3],i[4]]
+                L.append(temp_list)
+            print(tabulate((L),headers="firstrow",tablefmt="psql"))
 
     else:
         print("Invalid Input.")
     print()
+    
 
 
 print("------------------Welcome Adminstrator------------------")
@@ -231,24 +290,25 @@ while True:
     print("5. Search for a order.")
     print("6. Exit")
     print()
-    choice = (int(input("Enter your choice: ")))
     
-    if choice == 1:
+    choice = (input("Enter your choice: "))
+        
+    if choice == "1":
         Add_Product()
 
-    elif choice == 2:
+    elif choice == "2":
         Delete_Product()
 
-    elif choice == 3:
+    elif choice == "3":
         Update_Product()
     
-    elif choice == 4:
-        Check_Last_Orders()
+    elif choice == "4":
+        Check_Orders()
     
-    elif choice == 5:
+    elif choice == "5":
         Search_Order_Details()
 
-    elif choice == 6:
+    elif choice == "6":
         print("Exiting the program.")
         print("----------------------------------------------------------------")
         break
